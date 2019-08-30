8dee1802805b94813c9d6c4d2760ae51d19e47c4
Tgolds <759641842@qq.com>
2019-08-30 21:26:56 +0800

    add two

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Login-main">
+        <div class="Login-box"></div>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+
+
+<style lang="less">
+    .Login-main{
+        background-color: #2b5b6b;
+        height: 100%;
+    }
+    .Login-box{
+        width: 420px;
+        height: 300px;
+        border-radius: 3%;
+        background-color: #fff;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform:translate(-50%,-50%)
+    }
+</style>
