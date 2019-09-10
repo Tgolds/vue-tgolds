2918fac636b46749d67cd1a55a0d08b807cc0a4a
Tgolds <759641842@qq.com>
2019-09-10 10:43:53 +0800

    数据统计

---
+++ src/components/report/Report.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+<script>
+import echarts from "echarts";
+import _ from "lodash";
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: "用户来源"
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#E9EEF3"
+            }
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: "value"
+          }
+        ]
+      }
+    };
+  },
+  methods: {},
+  created() {},
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById("main"));
+
+    const { data: res } = await this.$http.get(`reports/type/1`);
+    if (res.meta.status !== 200) {
+      this.$message.error("获取数据失败");
+    }
+    //由于服务器返回的数据不完整需要对数据进行合并
+    const result = _.merge(res.data, this.options);
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result);
+  }
+};
+</script>   
